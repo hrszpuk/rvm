e9ac8a8a8d9e9837305cbdcdc4b46ecd2bfa90cf
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-24 12:40:47 +0000

    Implemented BytecodeToOpcode

--- src/lib/instruction.c
+++ src/lib/instruction.c
@@ -2,4 +2,17 @@
 // Created by hrs on 24/01/24.
 //
 
-#include "instruction.h"
+#include "headers/instruction.h"
+
+#include <string.h>
+
+Opcode BytecodeToOpcode(const char* bytecode) {
+    Opcode code = OPCODE_ERROR;
+    for(int i = 0; i < OPCODE_ENUM_LENGTH; i++) {
+        if (strcmp(OpcodeStringMap[i], bytecode) == 0) {
+            code = i;
+        }
+    }
+    return code;
+}
+
