950342bdcb35577bb5f14506b7428ed6f9ef04c8
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:02:33 +0000

    Buffer: get, set, add

--- src/lib/buffer.c
+++ src/lib/buffer.c
@@ -23,3 +23,28 @@ void DestroyBuffer(Buffer* buffer) {
     free(buffer);
 }
 
+void* GetBufferData(const Buffer* buffer, const int index) {
+    if (index < 0 || index >= buffer->count) {
+        return NULL;
+    }
+
+    return buffer->data[index];
+}
+
+void SetBufferData(const Buffer* buffer, const int index, void* element) {
+    if (index < 0 || index >= buffer->count) {
+        return;
+    }
+
+    buffer->data[index] = element;
+}
+
+void AddBufferData(Buffer* buffer, void* element) {
+    if (buffer->count >= buffer->capacity) {
+        buffer->capacity *= 2;
+        buffer->data = realloc(buffer->data, sizeof(void*) * buffer->capacity);
+    }
+
+    buffer->data[buffer->count] = element;
+    buffer->count++;
+}
