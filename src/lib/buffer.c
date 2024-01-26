772a703916b0af876d75a1e9b2a655f2e4218337
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:02:16 +0000

    Buffer: creation and destruction

--- src/lib/buffer.c
+++ src/lib/buffer.c
@@ -6,4 +6,20 @@
 
 #include <stdlib.h>
 
+Buffer* CreateBuffer(const int capacity) {
+    Buffer* buffer = malloc(sizeof(Buffer));
+    buffer->count = 0;
+    buffer->capacity = capacity;
+    buffer->data = malloc(sizeof(void*) * capacity);
+
+    return buffer;
+}
+
+void DestroyBuffer(Buffer* buffer) {
+    for (int i = 0; i < buffer->count; i++) {
+        free(buffer->data[i]);
+    }
+
+    free(buffer);
+}
 
