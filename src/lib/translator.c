eab737f842cb91e5a1352ad5f49d31ce182da72e
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:04:59 +0000

    Added TranslateInstructions

--- src/lib/translator.c
+++ src/lib/translator.c
@@ -5,3 +5,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+
+Buffer* TranslateInstructions(char* instructions) {
+    const char* line = strtok(instructions, "\n");
+    Buffer* instr_buffer = CreateBuffer(1);
+
+    while (line != NULL) {
+        if (isalpha(line[0])) {
+            TranslationResult result = TranslateInstruction(line);
+            AddBufferData(instr_buffer, &result);
+        }
+
+        line = strtok(NULL, "\n");
+    }
+
+    return instr_buffer;
+}
