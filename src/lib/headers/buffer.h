62b58dd9cac0e137b2b9f14a66f06dfec5169a28
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:00:55 +0000

    Added buffer.h

---
+++ src/lib/headers/buffer.h
@@ -0,0 +1,16 @@
+//
+// Created by hrs on 26/01/24.
+//
+
+#ifndef BUFFER_H
+#define BUFFER_H
+
+typedef struct {
+    int count;
+    int capacity;
+    void** data;
+} Buffer;
+
+
+
+#endif //BUFFER_H
