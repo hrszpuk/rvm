4e4a59593d518771498c31a34557c329b55e8f92
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 15:16:43 +0000

    Added help methods

--- src/cli/main.c
+++ src/cli/main.c
@@ -1,6 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
-#include "../lib/headers/stack.h"
+void HelpBasic(); // displays a basic help menu (similar to #11)
+void HelpRun(); // displays a help menu for the run command
+void HelpBuild(); // displays a help menu for the build command
+void HelpCheck(); // displays a help menu for the check command
+void HelpHelp(); // displays a help menu for the help command
 
 int main(int argc, char** argv) {
     return 0;
