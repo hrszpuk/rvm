7cdd8322ade10327aa72df5ed7dbdd4003444890
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:05:43 +0000

    Added TranslateFile

--- src/lib/translator.c
+++ src/lib/translator.c
@@ -23,7 +23,6 @@ Buffer* TranslateInstructions(char* instructions) {
     return instr_buffer;
 }
 
-
 // NOTE: instructions are in the form "<instruction> <arg>". Addional arguments are stored on the stack.
 // i.e. "push 5" will push 5 onto the stack, "add" will add the top two values on the stack.
 // Return format: first character is the opcode, the rest are the arguments, if any. End with a \n terminator.
@@ -62,4 +61,34 @@ TranslationResult TranslateInstruction(char* instruction) {
     free(args);
 
     return (TranslationResult) { result, args_size + 1, success };
+}
+
+
+void TranslateFile(const char* filename, const char* output_filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: could not open file %s\n", filename);
+        return;
+    }
+
+    fseek(file, 0, SEEK_END);
+    const long file_size = ftell(file);
+    rewind(file);
+
+    char* file_contents = malloc(sizeof(char) * file_size + 1);
+    fread(file_contents, sizeof(char), file_size, file);
+    file_contents[file_size] = '\0';
+
+    const Buffer* buffer = TranslateInstructions(file_contents);
+
+    free(file_contents);
+    fclose(file);
+    file = fopen(output_filename, "w");
+
+    for (int i = 0; i < buffer->count; i++) {
+        const TranslationResult* result = GetBufferData(buffer, i);
+        fwrite(result->bytecode, sizeof(char), result->length, file);
+    }
+
+    printf("Translation complete.\n");
 }
