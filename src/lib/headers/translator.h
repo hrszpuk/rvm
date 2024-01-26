44f2f53c3a0277f41dc8641cb2185edcbfbf37f5
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:03:52 +0000

    Added BytecodeMap (char*)

--- src/lib/headers/translator.h
+++ src/lib/headers/translator.h
@@ -7,4 +7,69 @@
 #define BYTECODE_INSTRUCTION_COUNT 40
 #include "headers/buffer.h"
 
+
+const char* BytecodeMap[BYTECODE_INSTRUCTION_COUNT] = {
+    // Misc
+    "halt",
+    "noop",
+
+    // Data operations
+    "load",
+    "store",
+    "vload",
+    "vstore",
+
+    // Stack operations
+    "push",
+    "pop",
+    "dup",
+    "swap",
+
+    // Arithmetic operations
+    "add",
+    "sub",
+    "div",
+    "mul",
+    "mod",
+    "inc",
+    "dec",
+
+    // Comparison operations
+    "eq",
+    "ne",
+    "lt",
+    "gt",
+    "le",
+    "ge",
+
+    // Logical operations
+    "land",
+    "lor",
+    "lnot",
+
+    // Bitwise operations
+    "and",
+    "or",
+    "xor",
+    "not",
+    "shl",
+    "shr",
+
+    // Control flow operations
+    // NOTE: labels are defined as "<name>:" in the source code. The translator will convert these to numbers.
+    "jmp",
+    "jmpc",
+
+    // Function operations
+    "call",
+    "ret",
+    "begin",
+    "end",
+
+    // I/O operations
+    "in",
+    "out",
+};
+
+
 #endif //TRANSLATOR_H
