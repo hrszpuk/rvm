84584a23ca87c26733833059e879788ac26d5121
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-26 14:05:22 +0000

    Added TranslateInstruction (used by TranslateInstructions)

--- src/lib/translator.c
+++ src/lib/translator.c
@@ -22,3 +22,44 @@ Buffer* TranslateInstructions(char* instructions) {
 
     return instr_buffer;
 }
+
+
+// NOTE: instructions are in the form "<instruction> <arg>". Addional arguments are stored on the stack.
+// i.e. "push 5" will push 5 onto the stack, "add" will add the top two values on the stack.
+// Return format: first character is the opcode, the rest are the arguments, if any. End with a \n terminator.
+TranslationResult TranslateInstruction(char* instruction) {
+    const char* instr = strtok(instruction, " ");
+    unsigned char opcode = 0;
+    char* args = malloc(sizeof(char) * 1);
+    int args_size = 1;
+
+    bool success = false;
+
+    for (int i = 0; i < BYTECODE_INSTRUCTION_COUNT; i++) {
+        if (strcmp(instr, BytecodeMap[i]) == 0) {
+            opcode = i;
+            success = true;
+            break;
+        }
+    }
+
+    instr = strtok(NULL, " ");
+    while (instr != NULL) {
+        args_size++;
+        args = realloc(args, sizeof(char) * args_size);
+        args[args_size - 1] = atoi(instr);
+        instr = strtok(NULL, " ");
+    }
+
+    args_size++;
+    args = realloc(args, sizeof(char) * args_size);
+    args[args_size - 1] = '\n';
+
+    char* result = malloc(sizeof(char) * args_size + 1);
+    result[0] = opcode;
+
+    memcpy(result + 1, args, args_size);
+    free(args);
+
+    return (TranslationResult) { result, args_size + 1, success };
+}
