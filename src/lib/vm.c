acb91b9564f7bcbe4ff6d5689cccd4f6f247731f
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 13:26:06 +0000

    Added main execution to RunVM

--- src/lib/vm.c
+++ src/lib/vm.c
@@ -33,6 +33,89 @@ void DestroyVM(VM* vm) {
 
 void RunVM(VM* vm) {
     vm->state = 0;
+
+    while (vm->state == 0) {
+        if (vm->ip >= vm->buffer->count) {
+            vm->state = 2;
+            break;
+        }
+
+        // Fetch instruction, apply it, and increment instruction pointer.
+        TranslationResult* instruction = GetBufferData(vm->buffer, vm->ip);
+        switch (instruction->bytecode[0]) {
+            case HALT: {
+                vm->state = 2;
+                break;
+            }
+            case NOOP: {
+                break;
+            }
+            case PUSH: {
+                int value = atoi(instruction->bytecode + 1);
+                PushStack(vm->stack, &value);
+                break;
+            }
+            case POP: {
+                PopStack(vm->stack);
+                break;
+            }
+            case ADD: {
+                int* a = PopStack(vm->stack);
+                int* b = PopStack(vm->stack);
+                int* result = malloc(sizeof(int));
+                *result = *a + *b;
+                PushStack(vm->stack, result);
+                free(a);
+                free(b);
+                break;
+            }
+            case SUB: {
+                int* a = PopStack(vm->stack);
+                int* b = PopStack(vm->stack);
+                int* result = malloc(sizeof(int));
+                *result = *a - *b;
+                PushStack(vm->stack, result);
+                free(a);
+                free(b);
+                break;
+            }
+            case DIV: {
+                int* a = PopStack(vm->stack);
+                int* b = PopStack(vm->stack);
+                int* result = malloc(sizeof(int));
+                *result = *a / *b;
+                PushStack(vm->stack, result);
+                free(a);
+                free(b);
+                break;
+            }
+            case MUL: {
+                int* a = PopStack(vm->stack);
+                int* b = PopStack(vm->stack);
+                int* result = malloc(sizeof(int));
+                *result = *a * *b;
+                PushStack(vm->stack, result);
+                free(a);
+                free(b);
+                break;
+            }
+            case MOD: {
+                int* a = PopStack(vm->stack);
+                int* b = PopStack(vm->stack);
+                int* result = malloc(sizeof(int));
+                *result = *a % *b;
+                PushStack(vm->stack, result);
+                free(a);
+                free(b);
+                break;
+            }
+            default: {
+                printf("Unknown instruction: %d\n", instruction->bytecode[0]);
+                break;
+            }
+        }
+        vm->ip++;
+    }
 }
 
 void PauseVM(VM* vm) {
