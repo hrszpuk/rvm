be3d39932897993ff997e426e548d356dac66f2d
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 12:02:50 +0000

    Created CreateDictEntry and FreeDictEntry

--- src/lib/dictionary.c
+++ src/lib/dictionary.c
@@ -3,3 +3,22 @@
 //
 
 #include "headers/dictionary.h"
+
+#include <stdlib.h>
+
+DictEntry* CreateDictEntry(char* key, char* value) {
+    DictEntry* entry = malloc(sizeof(DictEntry));
+    entry->key = key;
+    entry->value = value;
+    entry->next = NULL;
+    entry->hash = 0;
+    entry->used = 0;
+    return entry;
+}
+
+// NOTE(hrs): This function does not free the key or value.
+void FreeDictEntry(DictEntry* entry) {
+    free(entry);
+}
+
+
