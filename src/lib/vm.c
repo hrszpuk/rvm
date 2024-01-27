ec099c80cb661d5af9eebe2b1c957ea8bde10786
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 13:01:30 +0000

    CreateVM

--- src/lib/vm.c
+++ src/lib/vm.c
@@ -3,3 +3,20 @@
 //
 
 #include "headers/vm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+VM* CreateVM(const int stackCapacity, const int bufferCapacity) {
+    VM* vm = malloc(sizeof(VM));
+    vm->stack = CreateStack(stackCapacity);
+    vm->buffer = CreateBuffer(bufferCapacity);
+    vm->ip = 0;
+    vm->state = 0;
+    vm->debug = 0;
+    return vm;
+}
+
+
+
+
