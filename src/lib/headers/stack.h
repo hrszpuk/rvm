669048a21b6176e226e0cbf099baf79f863e658c
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 11:22:21 +0000

    Updated stack procedures

--- src/lib/headers/stack.h
+++ src/lib/headers/stack.h
@@ -12,9 +12,14 @@ typedef struct {
     int growthFactor;     // Default to 2 (NOTE: growth rate is always 1 * growthFactor)
 } Stack;
 
-Stack* CreateStack();
-void* PopStack();
-void PushStack(Stack*);
-void FreeStack(Stack*);
+Stack* CreateStack(int capacity);   // Create a stack with a given capacity
+void* PopStack(Stack*);                   // Pop the top element from the stack
+void PushStack(Stack*, void*);      // Push an element to the stack
+void FreeStack(Stack*);             // Free the stack
+void PrintStack(Stack*);            // Print the stack
+void GrowStack(Stack*);             // Grow the stack
+void ClearStack(Stack*);            // Clear the stack
+bool IsEmptyStack(Stack*);          // Check if the stack is empty
+void* TopStack(Stack*);              // Get the top element of the stack (without popping)
 
 #endif //STACK_H
