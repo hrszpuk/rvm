f4cae45bba6f80480a92300e5eadcb26b9d605d8
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 11:37:50 +0000

    Added heap procedures

--- src/lib/headers/heap.h
+++ src/lib/headers/heap.h
@@ -5,4 +5,19 @@
 #ifndef HEAP_H
 #define HEAP_H
 
+typedef struct {
+    void** data;
+    int size;
+    int capacity;
+} Heap;
+
+Heap* CreateHeap(int capacity);
+void DestroyHeap(Heap* heap);
+void InsertHeap(Heap* heap, void* data);
+void RemoveHeap(Heap* heap, void* data);
+void* ExtractMin(Heap* heap);
+void* PeekHeap(Heap* heap);
+void Heapify(Heap* heap, int index);
+void HeapSort(Heap* heap);
+
 #endif //HEAP_H
--- src/lib/heap.c
+++ src/lib/heap.c
@@ -3,3 +3,20 @@
 //
 
 #include "headers/heap.h"
+
+Heap* CreateHeap(int capacity);
+
+void DestroyHeap(Heap* heap);
+
+void InsertHeap(Heap* heap, void* data);
+
+void RemoveHeap(Heap* heap, void* data);
+
+void* ExtractMin(Heap* heap);
+
+void* PeekHeap(Heap* heap);
+
+void Heapify(Heap* heap, int index);
+
+void HeapSort(Heap* heap);
+
