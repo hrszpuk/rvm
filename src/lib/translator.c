307eabe04f3c0eca1b913d1ea19b94d2eb4e7597
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 14:56:08 +0000

    This is the worst C code I've ever written in my life.

--- src/lib/translator.c
+++ src/lib/translator.c
@@ -69,55 +69,67 @@ const char* BytecodeMap[BYTECODE_INSTRUCTION_COUNT] = {
 
 Buffer* TranslateInstructions(char* instructions) {
     Buffer* result = CreateBuffer(10);
-    char* line = strtok(instructions, "\n");
-    while (line != NULL) {
-        Instruction parsed = ParseInstruction(line);
-        AddBufferData(result, &parsed);
-        line = strtok(NULL, "\n");
+    int i = 0;
+    const int length = strlen(instructions);
+
+    char* arg = NULL, *instr = malloc(sizeof(char));
+    int instr_size = 1, arg_size = 1;
+    char* buffer = instr;
+    int* buffer_size = &instr_size;
+
+    while (i < length) {
+        char c = instructions[i];
+        buffer[*buffer_size - 1] = c;
+        buffer = realloc(buffer, sizeof(char) * ++*buffer_size);
+
+        if (c == '\n' || c == '\0') {
+            Instruction instruction = ConstructInstruction(instr, arg);
+            AddBufferData(result, &instruction);
+        } else if (c == ' ') {
+            arg = malloc(sizeof(char));
+            buffer = arg;
+            buffer_size = &arg_size;
+        }
+        i++;
     }
 
     printf("Buffer:\n");
-    for (int i = 0; i < result->count; i++) {
-        Instruction* instruction = GetBufferData(result, i);
+    for (int k = 0; k < result->count; k++) {
+        Instruction* instruction = GetBufferData(result, k);
         printf("%d %s\n", instruction->opcode, instruction->arg);
     }
     return result;
 }
 
-Instruction ParseInstruction(char* instruction) {
+// This function assumes that instruction is a single line of code.
+// It will return an Instruction struct with the opcode, argument, and type.
+Instruction ConstructInstruction(char* instr_literal, char* arg) {
     Instruction result;
     result.opcode = 1;
-    result.arg = NULL;
+    result.arg = arg;
     result.type = 0;
 
-    char* token = strtok(instruction, " ");
-    if (token == NULL) {
-        return result;
-    }
-
-    bool success = false;
     for (int i = 0; i < BYTECODE_INSTRUCTION_COUNT; i++) {
-        if (strcasecmp(token, BytecodeMap[i]) == 0) {
+        if (strcmp(instr_literal, BytecodeMap[i]) == 0) {
             result.opcode = i;
-            success = true;
             break;
         }
     }
-    printf("Found: %d (%d)\n", result.opcode, success);
 
-    if (!success) {
+    if (arg == NULL) {
         return result;
     }
 
-    token = strtok(NULL, " ");
-    if (token == NULL) {
-        return result;
+    // TODO: this type parsing can be improved.
+    if (isdigit(arg[0])) {
+        result.type = 1;
+    } else if (arg[0] == '\'') {
+        result.type = 2;
+    } else if (arg[0] == '"') {
+        result.type = 3;
+    } else {
+        result.type = 4;
     }
-
-    result.arg = token;
-    printf("Result: %d %s\n", result.opcode, result.arg);
-
-    result.type = 1;
 
     return result;
 }
