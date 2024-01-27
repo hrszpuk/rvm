b041ac1c8e683c6ce38ee59ed5d049198d590e1a
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-27 11:23:56 +0000

    Updated CreateStack

--- src/lib/stack.c
+++ src/lib/stack.c
@@ -4,9 +4,15 @@
 
 #include "headers/stack.h"
 
+#include <stdio.h>
 #include <stdlib.h>
 
-Stack* CreateStack() {
-    Stack* s = malloc(sizeof(Stack));
-    return s;
+Stack* CreateStack(int capacity) {
+    Stack* stack = malloc(sizeof(Stack));
+    stack->data = malloc(sizeof(void*) * capacity);
+    stack->capacity = capacity;
+    stack->count = 0;
+    stack->growthFactor = 2;
+    return stack;
+}
 }
