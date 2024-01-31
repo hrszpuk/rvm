92c96978b74038f408d58ab80c03a186a24084b5
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-31 14:59:56 +0000

    Added Token struct + methods

--- src/assembler/headers/token.h
+++ src/assembler/headers/token.h
@@ -5,4 +5,23 @@
 #ifndef TOKEN_H
 #define TOKEN_H
 
+typedef enum {
+    UNKNOWN,
+    END_OF_FILE,
+    END_OF_LINE,
+} TokenType;
+
+TokenType KeywordToTokenType(char* keyword); // Returns IDENTIFIER if the keyword is not recognised.
+
+typedef struct {
+    char* value;
+    int line;
+    int column;
+    TokenType type;
+} Token;
+
+Token* CreateToken(char* value, const int line, const int column, const TokenType type);
+void DestroyToken(Token* token);
+
+
 #endif //TOKEN_H
