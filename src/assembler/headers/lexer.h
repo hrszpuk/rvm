fc26a476b3025295e292ad09ab2a90b953fff3c8
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-31 14:59:47 +0000

    Added Lexer struct + methods

--- src/assembler/headers/lexer.h
+++ src/assembler/headers/lexer.h
@@ -5,4 +5,24 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <stdio.h>
+#include "token.h"
+
+typedef struct {
+    FILE* file;
+    char* buffer;
+    char* path;
+    Token* tokens;
+    int index;
+    int line;
+    int column;
+} Lexer;
+
+Lexer* CreateLexer();
+Lexer* CreateLexerFromFilename(const char* path);
+void DestroyLexer(Lexer* lexer);
+void DumpLexer(Lexer* lexer);
+void Lex(Lexer* lexer);
+
+
 #endif //LEXER_H
