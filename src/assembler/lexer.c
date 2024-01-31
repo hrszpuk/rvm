3535a635c5620d35e0db0edb814259f1522be716
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-01-31 15:24:51 +0000

    Added CreateLexer

--- src/assembler/lexer.c
+++ src/assembler/lexer.c
@@ -3,3 +3,17 @@
 //
 
 #include "headers/lexer.h"
+
+#include <stdlib.h>
+
+Lexer* CreateLexer() {
+    Lexer* lexer = malloc(sizeof(Lexer));
+    lexer->file = NULL;
+    lexer->buffer = NULL;
+    lexer->path = NULL;
+    lexer->tokens = NULL;
+    lexer->index = 0;
+    lexer->line = 1;
+    lexer->column = 1;
+    return lexer;
+}
