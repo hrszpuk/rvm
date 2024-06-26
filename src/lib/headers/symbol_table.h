9d9011e0b8910efa31417263dc6155a2d7f1e5f1
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-06-26 15:47:11 +0100

    Added symbol table

---
+++ src/lib/headers/symbol_table.h
@@ -0,0 +1,26 @@
+//
+// Created by hrs on 26/06/24.
+//
+
+#ifndef RVM_SYMBOL_TABLE_H
+#define RVM_SYMBOL_TABLE_H
+
+typedef enum {
+    INT
+} SymbolType;
+
+typedef struct {
+    char *name;
+    void* value;
+    SymbolType type;
+    int scope;
+} Symbol;
+
+Symbol CreateSymbol
+
+typedef struct {
+    Symbol* symbols;
+    int symbol_count;
+} SymbolTable;
+
+#endif //RVM_SYMBOL_TABLE_H
---
+++ src/lib/symbol_table.c
@@ -0,0 +1,5 @@
+//
+// Created by hrs on 26/06/24.
+//
+
+#include "symbol_table.h"
