4f5749d9e15379a98cc755ccefc4ea6d1606de19
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-06-26 13:28:11 +0100

    Fixed misaligned comment

--- src/lib/headers/stack.h
+++ src/lib/headers/stack.h
@@ -15,13 +15,13 @@ typedef struct {
 } Stack;
 
 Stack* CreateStack(int capacity);   // Create a stack with a given capacity
-void* PopStack(Stack*);                   // Pop the top element from the stack
+void* PopStack(Stack*);             // Pop the top element from the stack
 void PushStack(Stack*, void*);      // Push an element to the stack
 void FreeStack(Stack*);             // Free the stack
 void PrintStack(Stack*);            // Print the stack
 void GrowStack(Stack*);             // Grow the stack
 void ClearStack(Stack*);            // Clear the stack
 bool IsEmptyStack(Stack*);          // Check if the stack is empty
-void* TopStack(Stack*);              // Get the top element of the stack (without popping)
+void* TopStack(Stack*);             // Get the top element of the stack (without popping)
 
 #endif //STACK_H
