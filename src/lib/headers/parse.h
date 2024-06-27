8f70debbc38c6f6aaf26fa4c50cb20450e62eb18
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-06-27 19:57:28 +0100

    Added parser skeleton

---
+++ src/lib/headers/parse.h
@@ -0,0 +1,24 @@
+//
+// Created by hrs on 27/06/24.
+//
+
+#ifndef RVM_PARSE_H
+#define RVM_PARSE_H
+
+#include <stdlib.h>
+#include "buffer.h"
+
+typedef struct {
+    int index;
+    unsigned char* buffer;
+    int buffer_size;
+    InstructionBuffer* instructions;
+} Parser;
+
+Parser* create_parser(char* buffer, int buffer_size);
+void destroy_parser(Parser* p);
+void parse(Parser* p);
+void parseLine(Parser* p);
+
+
+#endif //RVM_PARSE_H
---
+++ src/lib/parse.c
@@ -0,0 +1,6 @@
+//
+// Created by hrs on 27/06/24.
+//
+
+#include "parse.h"
+
