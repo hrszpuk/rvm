95278bffe8b88847291a19bd9f0a75614c0e0710
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-06-27 19:57:42 +0100

    Implemented parser basics

--- src/lib/parse.c
+++ src/lib/parse.c
@@ -4,3 +4,28 @@
 
 #include "parse.h"
 
+Parser* create_parser(char* buffer, int buffer_size) {
+    Parser* p = malloc(sizeof(Parser));
+    p->index = 0;
+    p->buffer = buffer;
+    p->buffer_size = buffer_size;
+    p->instructions = CreateBuffer(20);
+    return p;
+}
+
+void destroy_parser(Parser* p) {
+    free(p);
+}
+
+void parse(Parser* p) {
+    while (p->index < p->buffer_size) {
+        Instruction instr = CreateInstruction(NOOP, NULL, IT_void);
+
+
+        if (HALT <= p->buffer[p->index] && p->buffer[p->index] < NUMBER_OF_OPCODE) {
+
+        } else if (p->buffer[p->index] == 255) {
+
+        }
+    }
+}
