892a7432c5b83031ef683e79907efa8d08392c9b
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-06-28 21:12:55 +0100

    The Stack can now handle more generic values

--- src/lib/headers/stack.h
+++ src/lib/headers/stack.h
@@ -6,22 +6,28 @@
 #define STACK_H
 
 #include <stdbool.h>
+#include "buffer.h"
 
 typedef struct {
-    void** data;
+    InstructionType type;
+    InstructionArgumentValue value;
+} StackValue;
+
+typedef struct {
+    StackValue* data;
     int capacity;
     int count;
     int growthFactor;     // Default to 2 (NOTE: growth rate is always 1 * growthFactor)
 } Stack;
 
 Stack* CreateStack(int capacity);   // Create a stack with a given capacity
-void* PopStack(Stack*);             // Pop the top element from the stack
-void PushStack(Stack*, void*);      // Push an element to the stack
+StackValue PopStack(Stack*);             // Pop the top element from the stack
+void PushStack(Stack*, StackValue);      // Push an element to the stack
 void FreeStack(Stack*);             // Free the stack
 void PrintStack(Stack*);            // Print the stack
 void GrowStack(Stack*);             // Grow the stack
 void ClearStack(Stack*);            // Clear the stack
 bool IsEmptyStack(Stack*);          // Check if the stack is empty
-void* TopStack(Stack*);             // Get the top element of the stack (without popping)
+StackValue TopStack(Stack*);             // Get the top element of the stack (without popping)
 
 #endif //STACK_H
--- src/lib/stack.c
+++ src/lib/stack.c
@@ -9,20 +9,20 @@
 
 Stack* CreateStack(int capacity) {
     Stack* stack = malloc(sizeof(Stack));
-    stack->data = malloc(sizeof(void*) * capacity);
+    stack->data = malloc(sizeof(StackValue) * capacity);
     stack->capacity = capacity;
     stack->count = 0;
     stack->growthFactor = 2;
     return stack;
 }
 
-void* PopStack(Stack* stack) {
-    void* data = TopStack(stack);
+StackValue PopStack(Stack* stack) {
+    StackValue data = TopStack(stack);
     stack->count--;
     return data;
 }
 
-void PushStack(Stack* stack, void* data) {
+void PushStack(Stack* stack, StackValue data) {
     if (stack->count == stack->capacity) {
         GrowStack(stack);
     }
@@ -31,9 +31,6 @@ void PushStack(Stack* stack, void* data) {
 }
 
 void FreeStack(Stack* stack) {
-    for (int i = 0; i < stack->count; i++) {
-        free(stack->data[i]);
-    }
     free(stack->data);
     free(stack);
 }
@@ -41,14 +38,14 @@ void FreeStack(Stack* stack) {
 void PrintStack(Stack* stack) {
     printf("Stack dump:\n");
     for (int i = 0; i < stack->count; i++) {
-        printf("%d: %p %d\n", i, stack->data[i], *(int*)stack->data[i]);
+        printf("%d: %p %d\n", i, stack->data + i, stack->data[i].value.i32);
     }
     printf("Total stack size: %d (capacity %d)\n", stack->count, stack->capacity);
 }
 
 void GrowStack(Stack* stack) {
     stack->capacity *= stack->growthFactor;
-    stack->data = realloc(stack->data, sizeof(void*) * stack->capacity);
+    stack->data = realloc(stack->data, sizeof(StackValue) * stack->capacity);
 }
 
 void ClearStack(Stack* stack) {
@@ -59,9 +56,9 @@ bool IsEmptyStack(Stack* stack) {
     return stack->count == 0;
 }
 
-void* TopStack(Stack* stack) {
+StackValue TopStack(Stack* stack) {
     if (stack->count == 0) {
-        return NULL;
+        return (StackValue){IT_void, {.i32 = 0}};
     }
     return stack->data[stack->count - 1];
 }
