48ebb9de2dda83f720f289132d6d43b0060a7be5
hrszpuk <107559570+hrszpuk@users.noreply.github.com>
2024-08-08 01:35:10 +0100

    Fixed warning

--- src/ras/headers/ras.h
+++ src/ras/headers/ras.h
@@ -1,28 +1,10 @@
-//
-// Created by hrs on 26/01/24.
-//
-
 #ifndef TRANSLATOR_H
 #define TRANSLATOR_H
 
 #define BYTECODE_INSTRUCTION_COUNT 13
 
 // Look up the assembly of a bytecode value: BytecodeMap[bytecode value]
-extern const char* BytecodeMap[BYTECODE_INSTRUCTION_COUNT] = {
-    "halt",
-    "noop",
-    "load",
-    "store",
-    "push",
-    "pop",
-    "add",
-    "sub",
-    "mul",
-    "div",
-    "alloc",
-    "free",
-    "print"
-};
+extern const char* BytecodeMap[BYTECODE_INSTRUCTION_COUNT];
 
 unsigned char assembly_to_bytecode(char*);
 
--- src/ras/ras.c
+++ src/ras/ras.c
@@ -2,6 +2,22 @@
 
 #include <string.h>
 
+const char* BytecodeMap[BYTECODE_INSTRUCTION_COUNT] = {
+    "halt",
+    "noop",
+    "load",
+    "store",
+    "push",
+    "pop",
+    "add",
+    "sub",
+    "mul",
+    "div",
+    "alloc",
+    "free",
+    "print"
+};
+
 unsigned char assembly_to_bytecode(char* instruction) {
     for (int i = 0; i < BYTECODE_INSTRUCTION_COUNT; i++) {
         if (strcmp(instruction, BytecodeMap[i]) == 0) {
